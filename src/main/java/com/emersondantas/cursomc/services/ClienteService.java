0cb08d889056c4fc981cded175dec5fc0633115a
Emerson Dantas <emerson.ruan@dce.ufpb.br>
2019-05-27 23:00:52 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/emersondantas/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.emersondantas.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emersondantas.cursomc.domain.Cliente;
+import com.emersondantas.cursomc.repositories.ClienteRepository;
+import com.emersondantas.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired //Automaticamente instanciado pelo spring por injeção de dependência
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) throws ObjectNotFoundException{
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
