3df7655273e243519ace7bb97e8d403b5f0ab0d2
Emerson Dantas <emerson.ruan@dce.ufpb.br>
2019-06-22 14:23:08 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/emersondantas/cursomc/resources/PedidoResource.java
@@ -0,0 +1,25 @@
+package com.emersondantas.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.emersondantas.cursomc.domain.Pedido;
+import com.emersondantas.cursomc.services.PedidoService;
+import com.emersondantas.cursomc.services.exceptions.ObjectNotFoundException;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	@Autowired
+	private PedidoService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
+		Pedido obg = service.buscar(id);
+		return ResponseEntity.ok().body(obg);
+	}
+}
---
+++ src/main/java/com/emersondantas/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.emersondantas.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emersondantas.cursomc.domain.Pedido;
+import com.emersondantas.cursomc.repositories.PedidoRepository;
+import com.emersondantas.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired //Automaticamente instanciado pelo spring por injeção de dependência
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) throws ObjectNotFoundException{
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
