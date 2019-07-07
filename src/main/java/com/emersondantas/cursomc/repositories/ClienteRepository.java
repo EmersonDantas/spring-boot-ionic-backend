1bd533f215940497b4cfdaeb1c8c05cdd6fc54f4
Emerson Dantas <emerson.ruan@dce.ufpb.br>
2019-07-07 15:22:24 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/emersondantas/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/emersondantas/cursomc/repositories/ClienteRepository.java
@@ -2,11 +2,13 @@ package com.emersondantas.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.emersondantas.cursomc.domain.Cliente;
-import com.emersondantas.cursomc.domain.Estado;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
