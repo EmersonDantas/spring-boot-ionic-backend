20bd608a23b23440f84150c8f2cfd784563d6069
Emerson Dantas <emerson.ruan@dce.ufpb.br>
2019-07-07 00:38:07 -0300

    Paginação comparametros opcionais na requisição

--- src/main/java/com/emersondantas/cursomc/services/CategoriaService.java
+++ src/main/java/com/emersondantas/cursomc/services/CategoriaService.java
@@ -1,17 +1,19 @@
 package com.emersondantas.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.emersondantas.cursomc.domain.Categoria;
 import com.emersondantas.cursomc.repositories.CategoriaRepository;
 import com.emersondantas.cursomc.services.exceptions.DataIntegrityException;
 import com.emersondantas.cursomc.services.exceptions.ObjectNotFoundException;
-
-import java.util.List;
 
 @Service
 public class CategoriaService {
@@ -46,5 +48,10 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
