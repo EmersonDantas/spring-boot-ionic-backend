e39ceda0f2334a1bc354f401d22bcc9962edce32
emersondantas <emerson.ruan@dce.ufpb.br>
2020-01-12 00:49:54 -0300

    Esqueci a senha implementado

--- src/main/java/com/emersondantas/cursomc/resources/AuthResource.java
+++ src/main/java/com/emersondantas/cursomc/resources/AuthResource.java
@@ -1,16 +1,21 @@
 package com.emersondantas.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.emersondantas.cursomc.dto.EmailDTO;
 import com.emersondantas.cursomc.security.JWTUtil;
 import com.emersondantas.cursomc.security.UserSS;
+import com.emersondantas.cursomc.services.AuthService;
 import com.emersondantas.cursomc.services.UserService;
+import com.emersondantas.cursomc.services.exceptions.ObjectNotFoundException;
 
 @RestController
 @RequestMapping(value = "/auth")
@@ -18,12 +23,21 @@ public class AuthResource {
 	
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private AuthService service;
 
 	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
 	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) throws ObjectNotFoundException {
+		service.sendNewPassword(objDto.getEmail());
 		return ResponseEntity.noContent().build();
 	}
 }
